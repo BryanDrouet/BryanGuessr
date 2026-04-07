facbda38b44fd68cf0aba9371918a4404f0c4848
Bryan_Drouet <77234743+BryanDrouet@users.noreply.github.com>
2026-04-07 17:29:49 +0000

    Fix URL formatting for fetching Pano data

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'fetchPanoData') {
-        const url = `https://www.google.com/maps/@?api=1&map_action=pano&pano=$${request.panoId}`;
+        // L'URL officielle pour forcer la redirection vers un Pano avec ses coordonnées GPS
+        const url = `https://www.google.com/maps/@?api=1&map_action=pano&pano=${request.panoId}`;
         
         fetch(url, { redirect: 'follow' })
             .then(response => {
